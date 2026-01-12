8ea3eefe70013e48b2d0da797c56549b4017c955
qings225 <11619310@qq.com>
2026-01-12 10:46:38 +0800

    M#1

--- src/common.rs
+++ src/common.rs
@@ -1085,7 +1085,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://47.111.163.111:61114".to_owned()
 }
 
 #[inline]
